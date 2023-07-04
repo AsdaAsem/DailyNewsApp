4d97565f5ee3a3dafaaf77ec61771882a6d4a4d6
Asda_Asem <75586948+AsdaAsem@users.noreply.github.com>
2023-07-04 18:40:05 +0200

    Add files via upload

---
+++ CoreApiStarter/Api/Services/Token/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using API.Models.Auth;
+
+namespace API.Services.Token
+{
+    public interface ITokenService
+    {
+        Task<string> CreateToken(AppUser user);
+    }
+}
